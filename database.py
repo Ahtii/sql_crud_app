c0ec722914e9750c30c6bd691d4e6ffd034df94b
Ahtisham <ahtishamshafi9906@gmail.com>
2020-11-24 20:07:04 +0530

    modified database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = "root"           # your db username
-DB_PASSWORD = "ahtisham"   # your db password
-DB = "applied_informatics"  # your database name
+DB_USER = "your-db-user"           # your db username
+DB_PASSWORD = "you-db-password"   # your db password
+DB = "your-db-name"  # your database name
 
 # connecting to mysql database
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://"+DB_USER+":"+DB_PASSWORD+"@localhost/"+DB
